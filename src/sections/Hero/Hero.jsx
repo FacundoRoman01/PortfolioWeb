d3bf3f757624eb53be6526e6b7d0d67b4671356c
facundo roman <facundoagustinroman01@gmail.com>
2024-05-30 12:10:50 -0300

    cambios para el portfolio

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/Design uten navn.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
+import instagramLight from '../../assets/instagram-light.svg';
+import instagramDark from '../../assets/instagram-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
@@ -15,7 +15,7 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const instagramIcon = theme === 'light' ? instagramLight : instagramDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -35,16 +35,16 @@ function Hero() {
         />
       </div>
       <div className={styles.info}>
-        <h1>
+        <h1 className='"tracking-in-expand"'>
           Facundo
           <br />
-          Roman
+          <span className='color-apellid'>Roman</span>
         </h1>
         <h2>Software Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
-          </a>
+        <a href="https://www.instagram.com/miblogcode/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramIcon} alt="Instagram icon" />
+        </a>
           <a href="https://github.com/FacundoRoman01" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
